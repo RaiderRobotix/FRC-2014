d6b81bb43f2305d2c3e1fa3b3caa50727395b16d
Raider Robotix <frcteam25@gmail.com>
2014-02-23 17:27:01 -0500

    Forgot to commit AutonController. End of unbag time.

--- src/edu/wpi/first/wpilibj/templates/AutonController.java
+++ src/edu/wpi/first/wpilibj/templates/AutonController.java
@@ -17,12 +17,12 @@ public class AutonController {
     private static AutonController m_instance;
     private int m_step;
     
-    private Drivebase m_drivebase;
-    private Pickup m_pickup;
+    private final Drivebase m_drivebase;
+    private final Pickup m_pickup;
     
     private boolean m_driveComplete;
     
-    private Timer m_timer;
+    private final Timer m_timer;
     private double m_ms;
     
     private AutonController() {
@@ -47,6 +47,10 @@ public class AutonController {
         m_drivebase.resetEncoders();
     }
     
+    public void DoNothing() {
+        m_drivebase.setSpeed(0.0);
+    }
+    
     public void PrintTime() {
         if (m_step == 0) {
             m_timer.start();
@@ -64,8 +68,9 @@ public class AutonController {
     public void DirtySanchezJr(boolean timeToScore) {
         //System.out.println("Auton step: " + m_step);
         if (m_step == 0) {
-            m_driveComplete = m_drivebase.driveForward(-185.0, 1.0);
+            m_driveComplete = m_drivebase.driveStraight(-185.0, 1.0, -90);
             if (m_driveComplete) {
+                m_driveComplete = false;
                 m_timer.start();
                 m_timer.reset();
                 m_step++;
@@ -87,4 +92,76 @@ public class AutonController {
             m_drivebase.setSpeed(0.0);
         }
     }
-} 
+    
+    public void DriveStraight() {
+        System.out.println("Distance: " + m_drivebase.getLeftEncoderDistance());
+        if (m_step == 0) {
+            m_driveComplete = m_drivebase.driveStraight(-185.0, 1.0, -90);
+            if (m_driveComplete) {
+                m_driveComplete = false;
+                m_step++;
+            }
+        } 
+        else {
+            m_drivebase.setSpeed(0.0);
+        }
+    }
+    
+    public void DriveStraightDeadReckon(boolean timeToScore) {
+        double distance = m_drivebase.getLeftEncoderDistance();
+        System.out.println("Distance: " + distance);
+        if (m_step == 0) {
+            m_drivebase.resetEncoders();
+            m_step++;
+        } 
+        else if (m_step == 1) {
+            if (distance > -5) {
+                m_drivebase.setSpeed(-0.30, -0.25);
+            } else {
+                m_step++;
+            }
+        }
+        else if (m_step == 2) {
+            if (distance > -100) {
+                m_drivebase.setSpeed(-1.0, -0.97);
+            } else {
+                m_step++;
+            }
+        }
+        else if (m_step == 3) {
+            if (distance > -140) {
+                m_drivebase.setSpeed(-0.5, -0.47);
+            } else {
+                m_step++;
+            }
+        }
+        else if (m_step == 4) {
+            if (distance > -160) {
+                 m_drivebase.setSpeed(-0.30, -0.25);
+            } else {
+                m_drivebase.setSpeed(0.0);
+                m_timer.start();
+                m_timer.reset();
+                m_step++;
+            }
+        }
+        else if (m_step == 5) {
+            m_drivebase.turnCameraLightOff();
+            m_pickup.openPickup();
+            if (m_timer.get() > 0.5) {
+                m_timer.reset();
+                m_step++;
+            }
+        }
+        else if (m_step == 6) {
+            if (timeToScore) {
+                m_pickup.runIntakeWheelsOut();
+                m_pickup.openKicker();
+                m_step++;
+            }
+        }
+        else {
+            m_drivebase.setSpeed(0.0);
+        }
+    }
+}  
